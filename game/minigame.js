6a5180b6aca7e22a29128651b11e62a8be954bb1
dartsia <nichisniukd.7b@gmail.com>
2024-03-26 21:01:10 +0200

    generating trees, resetting game, restarting game handling

--- game/minigame.js
+++ game/minigame.js
@@ -19,6 +19,105 @@ canvas.height = window.innerHeight;
 
 const ctx = canvas.getContext("2d");
 
+const introductionElement = document.getElementById("introduction");
+const perfectElement = document.getElementById("perfect");
+const restartButton = document.getElementById("restart");
+const scoreElement = document.getElementById("score");
+
+// Initialize layout
+resetGame();
+
+// Resets game variables and layouts but does not start the game (game starts on keypress)
+function resetGame() {
+    // Reset game progress
+    phase = "waiting";
+    lastTimestamp = undefined;
+    sceneOffset = 0;
+    score = 0;
+
+    introductionElement.style.opacity = 1;
+    perfectElement.style.opacity = 0;
+    restartButton.style.display = "none";
+    scoreElement.innerText = score;
+
+    // The first platform is always the same
+    // x + w has to match paddingX
+    platforms = [{ x: 50, w: 50 }];
+    generatePlatform();
+    generatePlatform();
+    generatePlatform();
+    generatePlatform();
+
+    sticks = [{ x: platforms[0].x + platforms[0].w, length: 0, rotation: 0 }];
+
+    trees = [];
+    generateTree();
+    generateTree();
+    generateTree();
+    generateTree();
+    generateTree();
+    generateTree();
+    generateTree();
+    generateTree();
+    generateTree();
+    generateTree();
+
+    heroX = platforms[0].x + platforms[0].w - heroDistanceFromEdge;
+    heroY = 0;
+
+    draw();
+}
+
+function generateTree() {
+    const minimumGap = 30;
+    const maximumGap = 150;
+
+    // X coordinate of the right edge of the furthest tree
+    const lastTree = trees[trees.length - 1];
+    let furthestX = lastTree ? lastTree.x : 0;
+
+    const x =
+        furthestX +
+        minimumGap +
+        Math.floor(Math.random() * (maximumGap - minimumGap));
+
+    const treeColors = ["#6D8821", "#8FAC34", "#98B333"];
+    const color = treeColors[Math.floor(Math.random() * 3)];
+
+    trees.push({ x, color });
+}
+
+function generatePlatform() {
+    const minimumGap = 40;
+    const maximumGap = 200;
+    const minimumWidth = 20;
+    const maximumWidth = 100;
+
+    // X coordinate of the right edge of the furthest platform
+    const lastPlatform = platforms[platforms.length - 1];
+    let furthestX = lastPlatform.x + lastPlatform.w;
+
+    const x =
+        furthestX +
+        minimumGap +
+        Math.floor(Math.random() * (maximumGap - minimumGap));
+    const w =
+        minimumWidth + Math.floor(Math.random() * (maximumWidth - minimumWidth));
+
+    platforms.push({ x, w });
+}
+
+resetGame();
+
+// If space was pressed restart the game
+window.addEventListener("keydown", function (event) {
+    if (event.key == " ") {
+        event.preventDefault();
+        resetGame();
+        return;
+    }
+});
+
 window.addEventListener("mousedown", function (event) {
     if (phase == "waiting") {
         lastTimestamp = undefined;
@@ -33,6 +132,14 @@ window.addEventListener("mouseup", function (event) {
         phase = "turning";
     }
 });
+
+window.addEventListener("resize", function (event) {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    draw();
+});
+
+window.requestAnimationFrame(animate);
 
 // The main game loop
 function animate(timestamp) {
@@ -132,6 +239,29 @@ function animate(timestamp) {
     window.requestAnimationFrame(animate);
 
     lastTimestamp = timestamp;
+}
+
+// Returns the platform the stick hit (if it didn't hit any stick then return undefined)
+function thePlatformTheStickHits() {
+    if (sticks.last().rotation != 90)
+        throw Error(`Stick is ${sticks.last().rotation}°`);
+    const stickFarX = sticks.last().x + sticks.last().length;
+
+    const platformTheStickHits = platforms.find(
+        (platform) => platform.x < stickFarX && stickFarX < platform.x + platform.w
+    );
+
+    // If the stick hits the perfect area
+    if (
+        platformTheStickHits &&
+        platformTheStickHits.x + platformTheStickHits.w / 2 - perfectAreaSize / 2 <
+        stickFarX &&
+        stickFarX <
+        platformTheStickHits.x + platformTheStickHits.w / 2 + perfectAreaSize / 2
+    )
+        return [platformTheStickHits, true];
+
+    return [platformTheStickHits, false];
 }
 
 function draw() {
@@ -249,15 +379,15 @@ function drawHero() {
 }
 
 function drawRoundedRect(x, y, width, height, radius) {
-  ctx.beginPath();
-  ctx.moveTo(x, y + radius);
-  ctx.lineTo(x, y + height - radius);
-  ctx.arcTo(x, y + height, x + radius, y + height, radius);
-  ctx.lineTo(x + width - radius, y + height);
-  ctx.arcTo(x + width, y + height, x + width, y + height - radius, radius);
-  ctx.lineTo(x + width, y + radius);
-  ctx.arcTo(x + width, y, x + width - radius, y, radius);
-  ctx.lineTo(x + radius, y);
-  ctx.arcTo(x, y, x, y + radius, radius);
-  ctx.fill();
+    ctx.beginPath();
+    ctx.moveTo(x, y + radius);
+    ctx.lineTo(x, y + height - radius);
+    ctx.arcTo(x, y + height, x + radius, y + height, radius);
+    ctx.lineTo(x + width - radius, y + height);
+    ctx.arcTo(x + width, y + height, x + width, y + height - radius, radius);
+    ctx.lineTo(x + width, y + radius);
+    ctx.arcTo(x + width, y, x + width - radius, y, radius);
+    ctx.lineTo(x + radius, y);
+    ctx.arcTo(x, y, x, y + radius, radius);
+    ctx.fill();
 }
