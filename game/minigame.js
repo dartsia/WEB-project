d9fcfb919d85f7787a86dd2db41b150a38254829
dartsia <nichisniukd.7b@gmail.com>
2024-03-26 18:49:37 +0200

    mantaining main loop of the game - building a stick bridge

--- game/minigame.js
+++ game/minigame.js
@@ -3,14 +3,115 @@ let lastTimestamp; // The timestamp of the previous requestAnimationFrame cycle
 
 window.addEventListener("mousedown", function (event) {
     if (phase == "waiting") {
-      lastTimestamp = undefined;
-      phase = "stretching";
-      window.requestAnimationFrame(animate);
+        lastTimestamp = undefined;
+        introductionElement.style.opacity = 0;
+        phase = "stretching";
+        window.requestAnimationFrame(animate);
     }
-  });
-  
-  window.addEventListener("mouseup", function (event) {
+});
+
+window.addEventListener("mouseup", function (event) {
     if (phase == "stretching") {
-      phase = "turning";
+        phase = "turning";
     }
-  });
+});
+
+// The main game loop
+function animate(timestamp) {
+    if (!lastTimestamp) {
+        lastTimestamp = timestamp;
+        window.requestAnimationFrame(animate);
+        return;
+    }
+
+    switch (phase) {
+        case "waiting":
+            return; // Stop the loop
+        case "stretching": {
+            sticks.last().length += (timestamp - lastTimestamp) / stretchingSpeed;
+            break;
+        }
+        case "turning": {
+            sticks.last().rotation += (timestamp - lastTimestamp) / turningSpeed;
+
+            if (sticks.last().rotation > 90) {
+                sticks.last().rotation = 90;
+
+                const [nextPlatform, perfectHit] = thePlatformTheStickHits();
+                if (nextPlatform) {
+                    // Increase score
+                    score += perfectHit ? 2 : 1;
+                    scoreElement.innerText = score;
+
+                    if (perfectHit) {
+                        perfectElement.style.opacity = 1;
+                        setTimeout(() => (perfectElement.style.opacity = 0), 1000);
+                    }
+
+                    generatePlatform();
+                    generateTree();
+                    generateTree();
+                }
+
+                phase = "walking";
+            }
+            break;
+        }
+        case "walking": {
+            heroX += (timestamp - lastTimestamp) / walkingSpeed;
+
+            const [nextPlatform] = thePlatformTheStickHits();
+            if (nextPlatform) {
+                // If hero will reach another platform then limit it's position at it's edge
+                const maxHeroX = nextPlatform.x + nextPlatform.w - heroDistanceFromEdge;
+                if (heroX > maxHeroX) {
+                    heroX = maxHeroX;
+                    phase = "transitioning";
+                }
+            } else {
+                // If hero won't reach another platform then limit it's position at the end of the pole
+                const maxHeroX = sticks.last().x + sticks.last().length + heroWidth;
+                if (heroX > maxHeroX) {
+                    heroX = maxHeroX;
+                    phase = "falling";
+                }
+            }
+            break;
+        }
+        case "transitioning": {
+            sceneOffset += (timestamp - lastTimestamp) / transitioningSpeed;
+
+            const [nextPlatform] = thePlatformTheStickHits();
+            if (sceneOffset > nextPlatform.x + nextPlatform.w - paddingX) {
+                // Add the next step
+                sticks.push({
+                    x: nextPlatform.x + nextPlatform.w,
+                    length: 0,
+                    rotation: 0
+                });
+                phase = "waiting";
+            }
+            break;
+        }
+        case "falling": {
+            if (sticks.last().rotation < 180)
+                sticks.last().rotation += (timestamp - lastTimestamp) / turningSpeed;
+
+            heroY += (timestamp - lastTimestamp) / fallingSpeed;
+            const maxHeroY =
+                platformHeight + 100 + (window.innerHeight - canvasHeight) / 2;
+            if (heroY > maxHeroY) {
+                restartButton.style.display = "block";
+                return;
+            }
+            break;
+        }
+        default:
+            throw Error("Wrong phase");
+    }
+
+    draw();
+    window.requestAnimationFrame(animate);
+
+    lastTimestamp = timestamp;
+}
